7c480ab0399568013d1dcf3b3998331a883a8e5f
upendra-23-cmd <vrambharat05@icloud.com>
2026-05-12 23:03:42 +0530

    new subnet configuration added

--- vpc/main.tf
+++ vpc/main.tf
@@ -21,27 +21,31 @@ resource "aws_vpc" "my_server_vpc" {
     }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_app" {
     cidr_block = "10.0.1.0/24"
     vpc_id = aws_vpc.my_server_vpc.id
     availability_zone = "us-east-1a"
+    tags = {
+        Name = "my-public-subnet"
+    }
+    map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet_db" {
     cidr_block = "10.0.2.0/24"
     vpc_id = aws_vpc.my_server_vpc.id
     availability_zone = "us-east-1b"
     tags = {
-        Name = "my-private-subnet"
+        Name = "my-private-subnet-database"
     }
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet_app" {
     cidr_block = "10.0.3.0/24"
     vpc_id = aws_vpc.my_server_vpc.id
     availability_zone = "us-east-1c"
     tags = {
-        Name = "my-private-subnet-database"
+        Name = "my-private-subnet-application"
     }
 }
 
@@ -52,9 +56,39 @@ resource "aws_internet_gateway" "my_igw" {
     }
 }
 
+resource "aws_eip" "nat_eip_app" {
+    domain = "vpc"
+    tags = {
+        Name = "my-nat-eip"
+    }
+}
+resource "aws_eip" "nat_eip_db" {
+    domain = "vpc"
+    tags = {
+        Name = "my-nat-eip"
+    }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.public_subnet_app.id
+    tags = {
+        Name = "my-nat-gateway"
+    }
+}
+
+resource "aws_nat_gateway" "nat_gateway_db" {
+    allocation_id = aws_eip.nat_eip.id
+    subnet_id = aws_subnet.private_subnet_app.id
+    tags = {
+        Name = "my-nat-gateway"
+    }
+}
+
+
 resource "aws_route_table" "my_rt" {
     vpc_id = aws_vpc.my_server_vpc.id
-    route = {
+    route  {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.my_igw.id
     }
@@ -63,8 +97,42 @@ resource "aws_route_table" "my_rt" {
     }
 }
 
+resource "aws_route_table" "my_private_route_table_db" {
+    vpc_id = aws_vpc.my_server_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gatewa_db.id
+    }
+  tags = {
+    Name = "my-private-route-table_db"
+  }
+}
+
+resource "aws_route_table_association" "my_private_rt_table_association_db" {
+    subnet_id =  aws_subnet.private_subnet_db.id
+    route_table_id = aws_route_table.my_private_route_table_db.id
+}
+
+resource "aws_route_table" "my_private_route_table" {
+    vpc_id = aws_vpc.my_server_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gateway.id
+    }
+  tags = {
+    Name = "my-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "my_private_rt_table_association" {
+    subnet_id =  aws_subnet.private_subnet_app.id
+    route_table_id = aws_route_table.my_private_route_table.id
+  
+}
+
 resource "aws_route_table_association" "my_rt_table_association" {
-    for_each = aws_subnet.public_subnet
+    subnet_id = aws_subnet.public_subnet.id
     route_table_id = aws_route_table.my_rt.id
   
 }
+
